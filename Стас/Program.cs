d9c757bc5773ad23625386a17ce50e57b7bbcd72
BeerZavr <danil.yakovlev6543@gmail.com>
2021-11-22 18:24:05 +0500

    1

---
+++ Стас/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Стас
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, C;
+            Console.Write("Введите a");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите b");
+            b = Convert.ToInt32(Console.ReadLine());
+            C = a + b;
+            Console.Write(C);
+            Console.ReadKey();
+        }
+    }
+}
